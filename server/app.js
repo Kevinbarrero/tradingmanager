907872d209e64755b88934afd5a8d154a93eb67b
holdandup~ <kevinbar98@gmail.com>
2023-04-11 17:04:57 +0300

    + Save | delete | strategies

--- server/app.js
+++ server/app.js
@@ -1,22 +1,23 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 let bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const auth = require("./middleware/auth");
 const bodyParser = require("body-parser");
 const db = require("./queries");
 const app = express();
+
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, _id, token"
   );
   next();
 });
@@ -65,6 +66,34 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.post("/saveStrategy", async (req, res) => {
+  console.log(req.headers);
+  try {
+    const { name, indicators, buyConditions, sellConditions } = req.body;
+    if (!(name && indicators && buyConditions && sellConditions)) {
+      res.status(400).send("All input is required");
+    } else {
+      const token = req.headers.token;
+      const user_id = req.headers._id;
+      if (!token && !user_id) {
+        res.status(401).send("Unauthorized");
+      } else {
+        const user = await User.findOne({ _id: user_id });
+        user.strategies.push({
+          name: name,
+          indicators: indicators,
+          buyConditions: buyConditions,
+          sellConditions: sellConditions,
+        });
+        await user.save();
+        res.status(200).send("Strategy saved successfully");
+      }
+    }
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+});
+
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -91,13 +120,46 @@ app.post("/login", async (req, res) => {
     console.log(err);
   }
 });
-
-/*
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
-*/
-app.get("/coins", db.getCoins)
+app.get("/getStrategies", async (req, res) => {
+  try {
+    const token = req.headers.token;
+    const user_id = req.headers._id;
+    if (!token && !user_id) {
+      res.status(401).send("Unauthorized");
+    } else {
+      const user = await User.findOne({ _id: user_id });
+      res.status(200).json(user.strategies);
+    }
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+});
+app.post("/deleteStrategy", async (req, res) => {
+  try {
+    const token = req.headers.token;
+    const user_id = req.headers._id;
+    const strategyid = req.body.strategy._id
+    if (!token && !user_id) {
+      res.status(401).send("Unauthorized");
+    } else {
+      User.findByIdAndUpdate(
+        user_id,
+        {
+          $pull: {
+            strategies: {
+              _id: strategyid,
+            },
+          },
+        },
+        { new: true }
+      )
+      res.status(200).send("Strategy Deleted")
+    }
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+});
+app.get("/coins", db.getCoins);
 app.get("/createtables", db.createTables);
 app.get("/klines/:coin/:interval/:startTime", db.getKlines);
 
